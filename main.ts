841b2af2cb8ba0bad6db29a88b17f4dde2566824
Harvey Lowden <harveyl1237@gmail.com>
2025-08-20 16:35:27 +0100

    finished historic calculations

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import * as readline from "readline";
-const prompt = require("prompt-sync")();
+import promptSync from "prompt-sync";
+const prompt = promptSync({ sigint: true });
 
 type Input = {
   start: Date;
@@ -23,45 +23,74 @@ type Calculation = {
   output: Output;
 };
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 function main() {
   let calculations: Calculation[] = [];
-  const option = prompt("create new (n) or use historic (h) transaction?");
-
-  if (option === "n") {
-    const input = getInput();
-    const output = calculateInterest(input);
-    console.log(output);
-  } else if (option === "h") {
-    console.log("historic");
-  } else {
-    const input: Input = {
-      start: new Date("2025-01-01"),
-      end: new Date("2025-07-02"),
-      amount: 1000,
-      currency: "GBP",
-      base: 0.045,
-      margin: 0.005,
-    };
-    const output = calculateInterest(input);
-    calculations.push({ input: input, output: output });
-    console.log(output);
+  let run = true;
+  while (run) {
+    let option = prompt(
+      "create new (n), use historic (h) transaction, use default (d), or quit (q)? "
+    );
+    if (option === "n") {
+      const [input, output] = newCalculation();
+      calculations.push({ input: input, output: output });
+      console.log(output);
+    } else if (option === "h") {
+      const [newInput, output] = historicCalculation(calculations);
+      console.log(output);
+      calculations.push({ input: newInput, output: output });
+    } else if (option === "q") {
+      run = false;
+    } else if (option === "d") {
+      const [input, output] = defaultCalculation();
+      console.log(output);
+      calculations.push({ input: input, output: output });
+    }
   }
-  return;
 }
 
-function getInput(): Input {
-  const startDate = new Date(prompt("What is the start date? (yyyy-mm-dd): "));
-  const endDate = new Date(prompt("What is the end date? (yyyy-mm-dd): "));
-  const amount: number = prompt("What is the loan amount?: ");
+function newCalculation(): [Input, Output] {
+  const input = getNewInput();
+  const output = calculateInterest(input);
+  return [input, output];
+}
+
+function historicCalculation(calculations: Calculation[]): [Input, Output] {
+  for (let i = 0; i < calculations.length; i++) {
+    printHistoric(calculations[i], i + 1);
+  }
+  let n: number = Number(prompt("pick a numbered option: "));
+  while (isNaN(n)) {
+    console.log(
+      "Invalid selection, please enter a number between 1 and " +
+        calculations.length
+    );
+    n = Number(prompt("pick a numbered option: "));
+  }
+  const newInput = getHistoricInput(calculations[n - 1].input);
+  const output = calculateInterest(newInput);
+  return [newInput, output];
+}
+
+function defaultCalculation(): [Input, Output] {
+  const input: Input = {
+    start: new Date("01/01/2025"),
+    end: new Date("07/02/2025"),
+    amount: 1000,
+    currency: "GBP",
+    base: 0.045,
+    margin: 0.005,
+  };
+  const output = calculateInterest(input);
+  return [input, output];
+}
+
+function getNewInput(): Input {
+  const startDate = new Date(prompt("What is the start date? (mm/dd/yyyy): "));
+  const endDate = new Date(prompt("What is the end date? (mm/dd/yyyy): "));
+  const amount: number = Number(prompt("What is the loan amount?: "));
   const currency: string = prompt("What is the loan currency?: ");
   const base: number = Number(prompt("What is the base interest rate?: "));
   const margin: number = Number(prompt("What is the margin interest rate?: "));
-
   const input: Input = {
     start: startDate,
     end: endDate,
@@ -72,6 +101,49 @@ function getInput(): Input {
   };
 
   return input;
+}
+
+function getHistoricInput(input: Input): Input {
+  console.log("Type updated input or press Enter to keep it the same:");
+  const startDate = new Date(
+    prompt(
+      "What is the start date? (mm/dd/yyyy): " +
+        "(" +
+        input.start.toLocaleDateString() +
+        "): "
+    )
+  );
+  const endDate = new Date(
+    prompt(
+      "What is the end date? (mm/dd/yyyy): " +
+        "(" +
+        input.end.toLocaleDateString() +
+        "): "
+    )
+  );
+  const amount = prompt(
+    "What is the loan amount? " + "(" + input.amount + "): "
+  );
+  const currency = prompt(
+    "What is the loan currency? " + "(" + input.currency + "): "
+  );
+  const base = prompt(
+    "What is the base interest rate? " + "(" + input.base + "): "
+  );
+  const margin = prompt(
+    "What is the margin interest rate? " + "(" + input.margin + "): "
+  );
+  // when user presses enter, blank return from prompt is ""
+  const newInput: Input = {
+    start: !isNaN(startDate.getTime()) ? startDate : input.start,
+    end: !isNaN(endDate.getTime()) ? endDate : input.end,
+    amount: amount ? Number(amount) : input.amount,
+    currency: currency || input.currency,
+    base: base ? Number(base) : input.base,
+    margin: margin ? Number(margin) : input.margin,
+  };
+
+  return newInput;
 }
 
 function calculateInterest(input: Input): Output {
@@ -94,6 +166,27 @@ function daysBetween(date1: Date, date2: Date): number {
   const day = 24 * 60 * 60 * 1000;
   const diffInDays = Math.ceil(diffInMilli / day).valueOf();
   return diffInDays;
+}
+
+function printHistoric(calculation: Calculation, n: number) {
+  console.log(
+    "[" +
+      n +
+      "]" +
+      " input: loan amount: " +
+      calculation.input.amount +
+      calculation.input.currency +
+      " at " +
+      (calculation.input.base + calculation.input.margin) * 100 +
+      "% total rate from " +
+      calculation.input.start.toLocaleDateString() +
+      " to " +
+      calculation.input.end.toLocaleDateString() +
+      "\n output: resulting in  " +
+      calculation.output.totalInterest.toFixed(2) +
+      calculation.input.currency +
+      " total interest."
+  );
 }
 
 main();
